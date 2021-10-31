e435c36c0a97bf8ffd198d7e66bbc613d7df3edd
Afzal Rahman <afzalrahman123@gmail.com>
2021-10-31 12:55:44 +0530

    Added remove tracking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
   get 'search_stock', to: 'stocks#search'
+  resources :friendships, only: [:create, :destroy]
 end
